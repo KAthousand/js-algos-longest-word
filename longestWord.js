615d4e9c57f52aeefc71cdf2c1bed202a9c27c32
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-10-06 17:30:10 -0400

    kyle thousand

--- longestWord.js
+++ longestWord.js
@@ -1,10 +1,15 @@
 //LONGEST WORD
 //return the longest word in a given array
-const longestWord = str => {
- 
-}
-console.log(longestWord("The quick brown fox jumped over the lazy dog"))
+
+const longestWord = (str) => {
+  const longestWord = str.split(" ").sort(function (a, b) {
+    return b.length - a.length;
+  });
+  return longestWord[0];
+};
+
+console.log(longestWord("The quick brown fox jumped over the lazy dog"));
 
 module.exports = {
-  longestWord
-}
+  longestWord,
+};
